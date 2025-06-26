d973e08da03570c4e8ddd956e6981bab20be5f40
Kaue Lima <kauelyma@gmail.com>
2025-06-26 20:43:22 -0300

    adicionando metores

--- TESTE/receitas.js
+++ TESTE/receitas.js
@@ -1,5 +1,2 @@
-let nomePessoa = "Lucia"
-let idadePessoa = 30
-let numerotelefone = "1234567890"
-let cpf = "123.456.789-00"
-let endereco = "Rua das Flores, 123"
+let veiculo = ["Fusca", "Caminhão", "Caminhonete", "Carro esportivo", "Van"];
+console.log(veiculo );
