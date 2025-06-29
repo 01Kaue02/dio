1768f87e86c759f46ab7e91741c25b879309714b
Kaue Lima <kauelyma@gmail.com>
2025-06-29 12:22:37 -0300

    alteração kmporlitro

--- TESTE/primeiro_desafio.js
+++ TESTE/primeiro_desafio.js
@@ -1,7 +1,7 @@
 console.log('Olá, mundo!');
 
 const valorCombustivel = 5.79;
-const kmPorLitro = 10;
+const kmPorLitro = 12.5; // Km por litro do carro
 const distanciaKm = 100;
 
 const listrosConsumidos = distanciaKm/ kmPorLitro; // Litros necessários
