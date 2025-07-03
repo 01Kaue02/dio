297d4c00e09cf665b70e602c6564daf880997274
Kaue Lima <kauelyma@gmail.com>
2025-07-03 19:11:44 -0300

    main

--- TESTE/imc.js
+++ TESTE/imc.js
@@ -1,17 +1,26 @@
+function calucularIMC(peso, altura) {
+  return peso / (altura * altura);
+}
+    function classificarIMC(imc) {
+        if (imc < 18.5) {
+        return("Abaixo do peso");
+} else if (imc >= 18.5 && imc < 24.9) {
+        return("Peso normal");
+} else if (imc >= 25 && imc < 29.9) {
+        return("Sobrepeso");
+} else if (imc >= 30 && imc < 34.9) {
+        return("Obesidade grau 1");
+} else if (imc >= 35 && imc <39.9) {
+    return("Obesidade grau 2");
+} else {
+    return("Obesidade grau 3");
+}
+    }
+    function main() {
 const peso= 75; // Peso em kg
 const altura = 1.60; // Altura em metros
-const imc = peso / (altura * altura); // Cálculo do IMC
-console.log(`Seu IMC é: ${imc.toFixed(2)}`); // Exibe o IMC com duas casas decimais
-if (imc < 18.5) {
-  console.log("Abaixo do peso");
-}   else if (imc >= 18.5 && imc < 24.9) {
-  console.log("Peso normal");
-} else if (imc >= 25 && imc < 29.9) {
-  console.log("Sobrepeso");
-} else if (imc >= 30 && imc < 34.9) {
-  console.log("Obesidade grau 1");
-} else if (imc >= 35 && imc <39.9) {
-    console.log("Obesidade grau 2");
-} else {
-  console.log("Obesidade grau 3");
-}
+const imc = calucularIMC(peso, altura); // Cálculo do IMC
+console.log(classificarIMC(imc)); // Exibe o IMC com duas casas decimais
+    }
+
+    main(); // Chama a função principal para executar o programa
