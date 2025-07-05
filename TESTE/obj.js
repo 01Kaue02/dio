f41b9e78a70f11e413d3eba711a942df35754fa1
Kaue Lima <kauelyma@gmail.com>
2025-07-05 11:40:43 -0300

    Funções recebendo Objetos

--- TESTE/obj.js
+++ TESTE/obj.js
@@ -16,11 +16,19 @@ class Pessoa{
  
     }
 }
-const kaue = new Pessoa('Kaue', 25);
+ function compararPessoas(p1, p2) {
+    if( p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho que ${p2.nome}.`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho que ${p1.nome}.`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} têm a mesma idade.`);
+    }
+ }
 
+ const vitor = new Pessoa("Vitor", 25 );
+    const maria = new Pessoa("Maria", 25);
 
-const Rennan = new Pessoa('Rennan', 30);
-console.log(kaue);
-console.log(Rennan);
+    compararPessoas(vitor, maria);
 
 
