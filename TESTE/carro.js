6649af76a340901c22c8f9204eec9d09ce824233
Kaue Lima <kauelyma@gmail.com>
2025-07-05 16:27:59 -0300

    calculando gasto

--- TESTE/carro.js
+++ TESTE/carro.js
@@ -8,4 +8,11 @@ class Carro{
         this.cor = cor;
         this.gastoMedioPorKm = gastoMedioPorKm;
     }
+    calcularGastoDePercurso(distanciaEmKm, precoCombustivel) {
+        return (distanciaEmKm * this.gastoMedioPorKm * precoCombustivel).toFixed(2);
 }
+}
+    const uno = new Carro("Fiat", "Preto", 1/12);
+     console.log( "Valor gasto foi: " + uno.calcularGastoDePercurso(70, 5)); // Exemplo de uso
+   const palio = new Carro("Fiat", "Vermelho", 1/10);
+   console.log("Valor gasto foi: " + palio.calcularGastoDePercurso(70, 5));
