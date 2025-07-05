51186c36317bb71d18f71a8154ff7ad2b32c2aa2
Kaue Lima <kauelyma@gmail.com>
2025-07-05 11:30:48 -0300

    adicionando constructor

--- TESTE/obj.js
+++ TESTE/obj.js
@@ -3,21 +3,24 @@ class Pessoa{
 
     nome;
     idade;
+    anoDeNascimento;
+    constructor (nome, idade, anoDeNascimento) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2025 - idade;
+        
+    }
 
      descrever ( ){
                console.log(`Meu nome é ${this.nome} e tenho ${this.idade} anos.`);
  
     }
 }
-const kaue = new Pessoa();
-kaue.nome = "Kaue";
-kaue.idade = 25;
+const kaue = new Pessoa('Kaue', 25);
 
 
-const Rennan = new Pessoa();
-Rennan.nome = "Rennan kdkd";
-Rennan.idade = 25;
-kaue.descrever();
-Rennan.descrever();
+const Rennan = new Pessoa('Rennan', 30);
+console.log(kaue);
+console.log(Rennan);
 
 
