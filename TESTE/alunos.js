d235baaa86f28107f598e3cb4fb86c8cfed6e10f
Kaue Lima <kauelyma@gmail.com>
2025-07-10 20:19:24 -0300

    notas

---
+++ TESTE/alunos.js
@@ -0,0 +1,5 @@
+const alunos = ['1', '2', '3', '4', '5']; // Array de aluno
+
+console.log(alunos); // Exibe o array original'
+console.log(alunos.shift()); // Exibe o array atualizado
+console.log(alunos); // Exibe o array atualizado
