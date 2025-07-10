9350631bc25034bc04f953bef3cd7e43d796d897
Kaue Lima <kauelyma@gmail.com>
2025-07-10 20:46:17 -0300

    for adicionado

---
+++ TESTE/77.js
@@ -0,0 +1,15 @@
+const nome= 'Vitor Santos daddada';
+
+
+
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+
+}
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);
+    
+}
