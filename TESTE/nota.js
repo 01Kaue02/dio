3953392e558048fceb80b023e52683015fb65bb5
Kaue Lima <kauelyma@gmail.com>
2025-07-10 21:15:11 -0300

    for nas notas

--- TESTE/nota.js
+++ TESTE/nota.js
@@ -6,5 +6,13 @@ notas.push(8);
 notas.push(10);
 notas.push(7);
 
-const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
-console.log (soma / 5);
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+    
+}
+
+const media = soma / notas.length;
+console.log(media);
